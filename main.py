14d1c917684c2538ac0edea773857a46a05ff7d7
Ishant Choudhary <codeitishant@gmail.com>
2025-05-01 10:50:28 +0530

    port change

--- main.py
+++ main.py
@@ -8,5 +8,5 @@ logger = logging.getLogger(__name__)
 logger.debug("Starting application...")
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # Using the PORT from environment variable
+    port = int(os.environ.get(5000))  # Using the PORT from environment variable
     # Normally, Gunicorn is run from the command line, not here in the code.
