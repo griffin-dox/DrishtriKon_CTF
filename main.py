d388daf219c87487d07381829a810ac5337fefce
Ishant Choudhary <codeitishant@gmail.com>
2025-06-16 01:10:30 +0530

    DEVOPS

--- main.py
+++ main.py
@@ -1,16 +1,7 @@
 from app import app  # Import the already configured app instance
-import logging
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-# Setup logging if needed
-logging.basicConfig(level=logging.DEBUG)
-logger = logging.getLogger(__name__)
-logger.debug("Starting application...")
 
 if __name__ == "__main__":
+    import os
     port = int(os.environ.get("PORT", 5000))
     debug = os.environ.get("FLASK_DEBUG", "0") == "1"
     app.run(host="0.0.0.0", port=port, debug=debug)
